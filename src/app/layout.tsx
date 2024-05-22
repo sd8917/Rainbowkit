99553b5ccb2651b22fa03e16a91ca8a923dd81ff
sudhanshu <sudhanshuraj8917@gmail.com>
2024-05-23 02:22:42 +0530

    FOOTER ADDED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from './providers';
 import { ThemeProvider } from "./_context/ThemeProvider";
 import NavBar from "./_components/NavBar";
+import Footer from "./_components/Footer"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             <Providers>
               <NavBar />
               {children}
+              <Footer />
             </Providers>
           </ThemeProvider>
       </body>
